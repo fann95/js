24010a9deb8aca7e650e1b14a4298472230d6951
fann95 <mobilhex@gmail.com>
2016-06-27 13:12:10 +0300

    up

--- port.js
+++ port.js
@@ -3,19 +3,17 @@ var url = require("url");
 var qs = require("querystring");
 var Tstamp='repeat';
 var Orders='';
-var MasterBalance='';
-var creator="https://www.mql5.com/ru/users/fan9"
+var creator="Dray Stanislav https://www.mql5.com/ru/users/fan9"
 
 
 function onRequest(request, response) {
 	var pathname = url.parse(request.url).pathname;
-	var params = qs.parse(pathname);
+    var params = qs.parse(pathname);
 	
 	if (params.side==='master') {                       //мастер
 		if(params.cmd==='update'){
 			Tstamp=params.Tstamp;
 			Orders=params.Orders;
-			MasterBalance=params.MasterBalance;
 			response.write('Accepted');
 			response.end();
 		}else if(params.cmd==='ping'){
@@ -26,21 +24,23 @@ function onRequest(request, response) {
 				response.write('isLive');
 				response.end();
 			}
+		}else{
+			response.write('repeat');
+			response.end();
 		}
 	}else if(params.side==='slave'){                    //клиент
-		if(params.Tstamp!==Tstamp && Orders!=''){
-			response.write(Orders+'*'+MasterBalance+'*'+Tstamp);
+      	if(params.Tstamp!==Tstamp && Orders!=''){
+			response.write(Orders+'*'+Tstamp);
 			response.end();
 		}else{
 			response.write('isLive');
 			response.end();
 		} 
 	}else{
-		//response.writeHead(404, {"Content-Type": "text/html"});
-		response.write("Technology by "+creator);
-		response.end();
-		
-	}
+        //response.writeHead(404, {"Content-Type": "text/html"});
+        response.write("Technology by "+creator);
+        response.end();
+    }
 
 }
 
